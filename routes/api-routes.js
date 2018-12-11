fa21600585923493eee322333ad4becde5bb6719
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-12-10 19:40:17 -0800

    now with more handlebars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ var cheerio = require("cheerio");
 const db = require("../models");
 
 module.exports = function (app) {
-    app.get('/', function(req, res) {
+    app.get('/api', function(req, res) {
         console.log('works');
     });
 }
--- server.js
+++ server.js
@@ -31,7 +31,14 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Start the server
