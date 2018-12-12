2ea863c6254f4a7ab125f3be9040c1e919cd3fbb
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-12-12 00:53:18 -0800

    working

---
+++ public/assets/js/app.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    $("#notes").hide();
+    scrapeAll();
+    renderArticles();
+});
+
+const scrapeAll = () => {
+    $.ajax("/api/scrape", {
+        type: "Get"
+    }).done(function (data) {
+        console.log(data);
+        renderArticles();
+    });
+
+}
+
+const renderArticles = () => {
+    $.getJSON("/api/all", function (articles) {
+        for (let i = 0; i < articles.length; i++) {
+            $(".articles").append("<h4>" + articles[i].title + "</h4><p>" + articles[i].body + "</p>" + "<a href='" + articles[i].link + "' target='_blank'>" + articles[i].link + "</a> <br /> <button data-id='" + articles[i]._id + "'type='button' class='btn btn-primary'>Add Note</button>");
+        }
+    });
+}
+
+$(document).on("click", "button", function () {
+    $("#notes").empty();
+    $("#notes").show();
+    let thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            $("#notes").append("<h2>" + data.title + "</h2>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+            if (data.note) {
+                $("#titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            }
+        });
+});
+
+let titleString;
+let bodyString;
+
+$(document).on("keyup", "#titleinput", function() {
+    titleString = $("#titleinput").val();
+});
+
+$(document).on("keyup", "#bodyinput", function() {
+    bodyString = $("#bodyinput").val();
+});
+
+$(document).on("click", "#savenote", function () {
+    let thisId = $(this).attr("data-id");
+    console.log(thisId);
+    console.log('title ' + titleString);
+    console.log('body ' + bodyString);
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: titleString,
+            body: bodyString
+        }
+    })
+        .then(function (data) {
+            console.log(data);
+            $("#notes").empty();
+        });
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+    titleString = '';
+    bodyString = '';
+    $("#notes").hide();
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ var cheerio = require("cheerio");
 const db = require("../models");
 
 module.exports = function (app) {
-    app.get("/scrape", function (req, res) {
+    app.get("/api/scrape", function (req, res) {
         // First, we grab the body of the html with axios
         axios.get("https://www.autoblog.com/news/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
@@ -31,17 +31,47 @@ module.exports = function (app) {
                 // Create a new Article using the `result` object built from scraping
                 db.Article.create(result)
                     .then(function (dbArticle) {
-                        // View the added result in the console
                         console.log(dbArticle);
                     })
                     .catch(function (err) {
-                        // If an error occurred, send it to the client
                         return res.json(err);
                     });
             });
-
-            // If we were able to successfully scrape and save an Article, send a message to the client
             res.send("Scrape Complete");
         });
+    });
+    // Get all articles in the db
+    app.get("/api/all", function (req, res) {
+        db.Article.find({})
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+    // Get article by id
+    app.get("/articles/:id", function (req, res) {
+        db.Article.findOne({ _id: req.params.id })
+            .populate("note")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+    // Post note to article
+    app.post("/articles/:id", function (req, res) {
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            })
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
     });
 }
